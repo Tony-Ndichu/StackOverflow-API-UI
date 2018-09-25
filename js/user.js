f8d7e355ef1668f4395bb6d1ec0f5d4a6c6b61a9
Tony-Wizadrop <ndichutony@gmail.com>
2018-09-25 10:33:48 +0300

    Fixed style.css

--- js/user.js
+++ js/user.js
@@ -18,8 +18,9 @@ const makeElement = (id, name, title, description, answers, parentId,  elementTy
           <p class="q-que-xtr fs-14">${description}</p>
 
           <div class="specs">
+          <div class="themecolor-text del-button " onClick="deleteQuestion(${id})" ><i class="fas fa-trash-alt fa-lg"></i></div>
             <div class="spec-answ answ themecolor-text"><i class="fas fa-pencil-alt"></i> ${answers} answers</div>
-            <div class="themecolor-text del-button " onClick="deleteQuestion(${id})" ><i class="fas fa-trash-alt fa-lg"></i></div>
+            
 
           </div>
           <div class="time">${time}</div>
