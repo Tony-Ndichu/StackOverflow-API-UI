2086963d294772f334591bdba805abe9ab573252
Tony-Wizadrop <ndichutony@gmail.com>
2018-09-25 10:32:18 +0300

    Fix bug in profile page

--- js/user.js
+++ js/user.js
@@ -19,7 +19,7 @@ const makeElement = (id, name, title, description, answers, parentId,  elementTy
 
           <div class="specs">
             <div class="spec-answ answ themecolor-text"><i class="fas fa-pencil-alt"></i> ${answers} answers</div>
-            <div class="themecolor-text del-button " onClick="deleteQuestion(${id})" >Delete<i class="fas fa-trash-alt fa-lg"></i></div>
+            <div class="themecolor-text del-button " onClick="deleteQuestion(${id})" ><i class="fas fa-trash-alt fa-lg"></i></div>
 
           </div>
           <div class="time">${time}</div>
