fbbe683d697c4e5d5a83b25297db045243987cee
Antony Ndichu <antonyndichu@Antonys-MacBook-Pro.local>
2018-10-03 19:26:16 +0300

    Fix bug in main.js

--- js/main.js
+++ js/main.js
@@ -122,11 +122,11 @@ window.onload = () =>{
 	hideMenu()
 	openModal()
 	closeModal()
-	if (window.location.pathname == "/UserProfile.html"){
+	if (window.location.pathname == "/Users/antonyndichu/Desktop/frontend/StackOverflow-API-UI/UserProfile.html"){
 	profilePage()
 }
 
-	if (window.location.pathname == "/ViewQuestion.html"){
+	if (window.location.pathname == "/Users/antonyndichu/Desktop/frontend/StackOverflow-API-UI/ViewQuestion.html"){
 	openQuestion()
 }
 }
